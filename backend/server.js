d32abdb327c0ed5f71732c56d101cc41e83f407e
Achmad Sofyan Pratama <fyans665@gmail.com>
2023-10-02 07:27:52 +0700

    first commit

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const app = express();
+const chats = require("./data/data");
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send(chats);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
